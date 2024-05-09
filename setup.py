52937eb6592eb3d7caa34355d7d4050600d7c3e7
YingtianDt <yingtian.tang@epfl.ch>
2024-05-09 21:53:06 +0200

    modify for brainscore

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Minimal setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='S3D_HowTo100M',
+    version='0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+    ]
+)
+
+
